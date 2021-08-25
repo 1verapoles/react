eec5b5edfd6d9e2f5fe5f1625b1df25b295e5a5a
1verapoles <47753672+1verapoles@users.noreply.github.com>
2021-08-25 11:45:25 +0200

    fix: fix error

--- src/PaginationComponent.jsx
+++ src/PaginationComponent.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-function PaginationComponent({ pageNumber, pageSize, totalResults, onChangePageSize, onChangePageNumber, onBlurPageSize, onBlurPageNumber }) {
+function PaginationComponent({ pageNumber, pageSize, pageSizeVal, totalResults, onChangePageSize, onChangePageNumber, onBlurPageSize, onBlurPageNumber }) {
   let total
   if (totalResults) {
-    total = Math.ceil(totalResults / pageSize)
+    total = Math.ceil(totalResults / pageSizeVal)
   } else { total = '' }
 
   return (
